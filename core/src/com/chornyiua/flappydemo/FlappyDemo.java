70b5dbd40ed95df3196d50e23f7892d1ad3f4b3c
y.chornyi <xym4er@gmail.com>
2016-12-25 18:07:20 +0300

    1

--- core/src/com/chornyiua/flappydemo/FlappyDemo.java
+++ core/src/com/chornyiua/flappydemo/FlappyDemo.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.chornyiua.flappydemo.states.GameStateManager;
+import com.chornyiua.flappydemo.states.MenuState;
 
 public class FlappyDemo extends ApplicationAdapter {
     public static final int WIDTH = 480;
@@ -20,6 +21,7 @@ public class FlappyDemo extends ApplicationAdapter {
         batch = new SpriteBatch();
         gsm = new GameStateManager();
         Gdx.gl.glClearColor(1, 0, 0, 1);
+        gsm.push(new MenuState(gsm));
     }
 
     @Override
