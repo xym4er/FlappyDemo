824956e8c76d82d0eb1e1145b305c9f65bde7491
y.chornyi <xym4er@gmail.com>
2016-12-27 18:31:58 +0300

    1

--- core/src/com/chornyiua/flappydemo/FlappyDemo.java
+++ core/src/com/chornyiua/flappydemo/FlappyDemo.java
@@ -5,19 +5,29 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.utils.ObjectMap;
+import com.badlogic.gdx.utils.ObjectSet;
 import com.chornyiua.flappydemo.states.GameStateManager;
 import com.chornyiua.flappydemo.states.MenuState;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 public class FlappyDemo extends ApplicationAdapter {
     public static final int WIDTH = 480;
     public static final int HEIGHT = 800;
     public static final String TITLE = "Flappy";
-
+    public static ObjectMap textures = new ObjectMap();
     private GameStateManager gsm;
     private SpriteBatch batch;
 
     @Override
     public void create() {
+//        TextureAtlas atlas = new TextureAtlas("awaesom.atlas");
+//        ObjectSet temp = atlas.getTextures();
+//        System.out.println(atlas.getRegions().first().getRegionHeight());
         batch = new SpriteBatch();
         gsm = new GameStateManager();
         Gdx.gl.glClearColor(1, 0, 0, 1);
--- desktop/src/com/chornyiua/flappydemo/desktop/DesktopLauncher.java
+++ desktop/src/com/chornyiua/flappydemo/desktop/DesktopLauncher.java
@@ -1,11 +1,19 @@
 package com.chornyiua.flappydemo.desktop;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker;
 import com.chornyiua.flappydemo.FlappyDemo;
 
 public class DesktopLauncher {
+
 	public static void main (String[] arg) {
+//        TextureAtlas atlas = new TextureAtlas("awaesom.atlas");
+//		System.out.println(atlas.getRegions().first().getRegionHeight());
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.width = FlappyDemo.WIDTH;
 		config.height = FlappyDemo.HEIGHT;
